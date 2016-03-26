36e1ecb00085d1698f4e8e11658a6e5493db78e0
Austin Hartzheim <austin@austinhartzheim.me>
2016-03-26 17:20:30 +0000

    Added text layers to display longest/current sequences.

--- src/utils/dataview.c
+++ src/utils/dataview.c
@@ -4,7 +4,8 @@
 #define CDC_CONSECUTIVE_RECOMMEND 10
 
 static char data_display[10];
-time_t start, stop;
+static char data_longest[12];
+static char data_current[12];
 
 
 void data_view_init(Layer *window_layer) {
@@ -28,10 +29,24 @@ void data_view_init(Layer *window_layer) {
   text_layer_set_font(tl_sub, fonts_get_system_font(FONT_KEY_GOTHIC_18));
   text_layer_set_text_alignment(tl_sub, GTextAlignmentCenter);
 
+  // Create longest duration text layer
+  tl_longest = text_layer_create(GRect(0, bounds.size.h/3*2, bounds.size.w, 22));
+  text_layer_set_text(tl_longest, "-");
+  text_layer_set_font(tl_longest, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+  text_layer_set_text_alignment(tl_longest, GTextAlignmentCenter);
+
+  // Create the current duration text layer
+  tl_current = text_layer_create(GRect(0, bounds.size.h/3*2 + 24, bounds.size.w, 22));
+  text_layer_set_text(tl_current, "-");
+  text_layer_set_font(tl_current, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+  text_layer_set_text_alignment(tl_current, GTextAlignmentCenter);
+
   // Add text layers to window
   layer_add_child(window_layer, text_layer_get_layer(tl_m_complete));
   layer_add_child(window_layer, text_layer_get_layer(tl_sub));
   layer_add_child(window_layer, text_layer_get_layer(tl_title));
+  layer_add_child(window_layer, text_layer_get_layer(tl_longest));
+  layer_add_child(window_layer, text_layer_get_layer(tl_current));
 }
 
 // Count the number of minutes of walking that qualify as part of a
@@ -39,10 +54,10 @@ void data_view_init(Layer *window_layer) {
 // Takes the start and end times to check as parameters.
 // Return -1 if malloc fails.
 // Return the total number of qualifying minutes.
-int calculate_qualifying_minutes(time_t start, time_t stop) {
+int calculate_qualifying_minutes(activity_summary_t *summary, time_t start, time_t stop) {
   HealthMinuteData *minute_data;
   int num_records;
-  
+
   minute_data = (HealthMinuteData*)malloc(MINUTES_PER_DAY*sizeof(HealthMinuteData));
   if (minute_data == NULL) {
     free(minute_data);
@@ -63,23 +78,37 @@ int calculate_qualifying_minutes(time_t start, time_t stop) {
       if (consecutive >= CDC_CONSECUTIVE_RECOMMEND) {
         total_minutes += consecutive;
       }
+      if (consecutive > 0)
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Found %d consecutive minutes.", consecutive);
+      if (consecutive > summary->max_consecutive)
+        summary->max_consecutive = consecutive;
       consecutive = 0;
     }
   }
-  
+
   free(minute_data);
-  return total_minutes;
+  summary->total_minutes = total_minutes;
+  summary->current = consecutive;
+  return 0;
 }
 
 
 void show_data_today() {
-  text_layer_set_text(tl_title, "Today");
-  time_t start = time_start_of_today();
-  time_t stop = time(NULL);
+  activity_summary_t summary;
+  summary.max_consecutive = 0;
+  
+  calculate_qualifying_minutes(&summary, time_start_of_today(), time(NULL));
 
-  int minutes = calculate_qualifying_minutes(start, stop);
-  snprintf(data_display, 10, "%d/30", minutes);
+  // Update text buffers
+  snprintf(data_display, 10, "%d/30", summary.total_minutes);
+  snprintf(data_longest, 12, "Longest: %d", summary.max_consecutive);
+  snprintf(data_current, 12, "Current: %d", summary.current);
+
+  // Set text layers
+  text_layer_set_text(tl_title, "Today");
   text_layer_set_text(tl_m_complete, data_display);
+  text_layer_set_text(tl_longest, data_longest);
+  text_layer_set_text(tl_current, data_current);
 }
 
 void show_data_week() {
--- src/utils/dataview.h
+++ src/utils/dataview.h
@@ -6,11 +6,19 @@ typedef enum {
   MODE_TODAY, MODE_WEEK
 } view_t;
 
+typedef struct _activity_summary {
+  int total_minutes;
+  int max_consecutive;
+  int current;
+} activity_summary_t;
+
 TextLayer *tl_m_complete;
 TextLayer *tl_title;
 TextLayer *tl_sub;
+TextLayer *tl_longest;
+TextLayer *tl_current;
 
 void data_view_init(Layer *);
-int calculate_qualifying_minutes(time_t, time_t);
+int calculate_qualifying_minutes(activity_summary_t *, time_t, time_t);
 void show_data_week();
 void show_data_today();
